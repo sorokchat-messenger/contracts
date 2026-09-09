0096b85dfbbca45345a615b2a9ffa06f82abd5a1
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-09 10:42:44 +0300

    feat: add users codes

---
+++ src/users/codes.ts
@@ -0,0 +1,32 @@
+import { ERROR_CODE } from "../common/index.js";
+
+const USER_ERROR: string = `${ERROR_CODE}.user`;
+const ERROR_LOGIN: string = `${USER_ERROR}.login`;
+const ERROR_PASSWORD: string = `${USER_ERROR}.password`;
+
+const MIN_LENGTH: number = 8;
+const MAX_LENGTH: number = 100;
+
+export const UserCodes = {
+  NOT_FOUND: `${USER_ERROR}.not-found`,
+  EXISTS: `${USER_ERROR}.exists`,
+  LOGIN: {
+    UNDEFINED: `${ERROR_LOGIN}.undefined`,
+    EMPTY: `${ERROR_LOGIN}.empty`,
+  },
+  PASSWORD: {
+    UNDEFINED: `${ERROR_PASSWORD}.undefined`,
+    EMPTY: `${ERROR_PASSWORD}.empty`,
+    MIN_LENGTH: {
+      VALUE: MIN_LENGTH,
+      code: `${ERROR_PASSWORD}.min-length`,
+    },
+    MAX_LENGTH: {
+      VALUE: MAX_LENGTH,
+      code: `${ERROR_PASSWORD}.max-length`,
+    },
+  },
+  DISPLAY_NAME: {
+    EMPTY: `${ERROR_PASSWORD}.empty`,
+  },
+} as const;
