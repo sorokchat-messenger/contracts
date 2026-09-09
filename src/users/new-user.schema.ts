4de68078ae4e4caacbd31c2b31ba64cb1c3ce4b0
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-09 19:43:59 +0300

    feat: openapi

--- src/users/new-user.schema.ts
+++ src/users/new-user.schema.ts
@@ -1,11 +1,18 @@
 import { UserCodes } from "./codes.js";
 import z from "zod";
+import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
+
+extendZodWithOpenApi(z);
 
 export const NewUserSchema = z.object({
   login: z
     .string()
     .nonempty({ message: UserCodes.LOGIN.EMPTY })
-    .nonoptional({ message: UserCodes.LOGIN.UNDEFINED }),
+    .nonoptional({ message: UserCodes.LOGIN.UNDEFINED })
+    .openapi({
+      description: "Унікальне ім'я користувача",
+      example: "sorokovsky_andrey",
+    }),
   password: z
     .string()
     .min(UserCodes.PASSWORD.MIN_LENGTH.VALUE, {
@@ -15,10 +22,20 @@ export const NewUserSchema = z.object({
       message: UserCodes.PASSWORD.MAX_LENGTH.CODE,
     })
     .nonempty({ message: UserCodes.PASSWORD.EMPTY })
-    .nonoptional({ message: UserCodes.PASSWORD.UNDEFINED }),
+    .nonoptional({ message: UserCodes.PASSWORD.UNDEFINED })
+    .openapi({
+      description: "Пароль користувача",
+      example: "<PASSWORD>",
+      minLength: UserCodes.PASSWORD.MIN_LENGTH.VALUE,
+      maxLength: UserCodes.PASSWORD.MAX_LENGTH.VALUE,
+    }),
   displayName: z
     .string()
     .nonempty({ message: UserCodes.DISPLAY_NAME.EMPTY })
-    .optional(),
+    .optional()
+    .openapi({
+      description: "Видиме ім'я користувача",
+      example: "Сороковський Андрій",
+    }),
 });
 export type NewUserPayload = z.infer<typeof NewUserSchema>;
