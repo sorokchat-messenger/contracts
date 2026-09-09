15412e36532321cc8ac0e9f2aea094f2feaad01d
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-09 18:21:43 +0300

    feat: authorization controller

---
+++ src/authorization/controller.ts
@@ -0,0 +1,8 @@
+export const AUTHORIZATION_CONTROLLER = {
+  NAME: "authorization",
+  REGISTER: "register",
+  LOGIN: "login",
+  LOGOUT: "logout",
+  PROFILE: "profile",
+  REFRESH_TOKENS: "refresh-tokens",
+} as const;
--- src/authorization/index.ts
+++ src/authorization/index.ts
@@ -1,3 +1,4 @@
 export * from "./codes.js";
 export * from "./register.schema.js";
 export * from "./login.schema.js";
+export * from "./controller.js";
