54e13fcf6bc8e35b9688342cc7fd55bcdf302de0
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-18 08:59:10 +0300

    fix: hirerarchy import roles

--- src/common/utils/role-hierarchy.util.ts
+++ src/common/utils/role-hierarchy.util.ts
@@ -1,5 +1,5 @@
-import { ChatRole } from "../chats/chat-role.js";
-import { Role } from "../users/role.js";
+import { ChatRole } from "../../chats/chat-role.js";
+import { Role } from "../../users/role.js";
 
 export class RoleHierarchy {
   private readonly map: Map<string, string[]>;
